36125ae91d8554d03c17587698a45107357d5dd4
Mahmoud El-Tohamy <mahmoud.eltohamyy@gmail.com>
2026-06-13 12:18:35 +0300

    Add Post resource with CRUD operations and URL comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,29 @@
 class PostsController < ApplicationController
-  # your code goes here
+  # GET /posts
+  def index
+  end
+
+  # GET /posts/:id
+  def show
+  end
+
+  # GET /posts/new
+  def new
+  end
+
+  # POST /posts/commit
+  def create
+  end
+
+  # GET /posts/:id/edit
+  def edit
+  end
+
+  # PATCH/PUT /posts/:id
+  def update
+  end
+
+  # DELETE /posts/:id
+  def destroy
+  end
 end
