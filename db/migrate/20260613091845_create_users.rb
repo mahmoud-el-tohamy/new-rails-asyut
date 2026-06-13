788a89b3672f7830e125fad59dfec6774b12adb1
Mahmoud El-Tohamy <mahmoud.eltohamyy@gmail.com>
2026-06-13 12:25:06 +0300

    Add address column to users with NOT NULL constraint

--- db/migrate/20260613091845_create_users.rb
+++ db/migrate/20260613091845_create_users.rb
@@ -1,12 +1,13 @@
-class CreateUsers < ActiveRecord::Migration[8.1]
+class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
       t.date :DOB
       t.string :email
       t.string :phone_number
+
       t.timestamps
-      add_index :users, :email, unique: true
     end
+    add_index :users, :email, unique: true
   end
 end
