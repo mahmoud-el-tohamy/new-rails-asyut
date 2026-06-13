109bb17b16c643786a1f428f30d1b25b567f8b32
Mahmoud El-Tohamy <mahmoud.eltohamyy@gmail.com>
2026-06-13 13:20:24 +0300

    feat: implement CRUD actions and helper methods in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy ]
+
   # GET /posts
   def index
+    @posts = Post.all
   end
 
   # GET /posts/:id
@@ -9,10 +12,18 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
+    @post = Post.new
   end
 
   # POST /posts/commit
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # GET /posts/:id/edit
@@ -21,9 +32,26 @@ class PostsController < ApplicationController
 
   # PATCH/PUT /posts/:id
   def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   # DELETE /posts/:id
   def destroy
+    @post.destroy
+    redirect_to posts_url
   end
+
+  private
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
